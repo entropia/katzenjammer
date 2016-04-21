c399b58c403acced1fab9e2f4d75825d73db2ddd
Peter Brantsch <peter@brantsch.name>
2016-04-21 09:36:57 +0200

    REFACTOR: use table instead of elseifs

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ PIN3 = 7
 function button_init ()
   gpio.trig(BUTTON, "down", button_callback)
   gpio.mode(BUTTON, gpio.INT)
-  
+
   gpio.mode(PIN1, gpio.INPUT, gpio.PULLUP)
 
   gpio.mode(PIN2, gpio.INPUT, gpio.PULLUP)
@@ -16,27 +16,22 @@ end
 
 
 
-function rimshot ()
+function trigger_effect ()
   g = gpio.read(PIN1) * 255
   r = gpio.read(PIN2) * 255
   b = gpio.read(PIN3) * 255
-  leds_grb = string.char(g,r,b, g,r,b) 
+  leds_grb = string.char(g,r,b, g,r,b)
   ws2812.write(2, leds_grb)
   socket = net.createConnection(net.UDP, 0)
   socket:connect(23421, "192.168.23.42")
-  
-  if (r == 255 and g == 0 and b == 0) then 
-    socket:send("haha\n")
 
-  elseif (r == 0 and g == 255 and b == 0) then
-    socket:send("fail\n")
-
-  elseif (r == 0 and g == 0 and b == 255) then
-    socket:send("shutdown\n")
-
-  else
-    socket:send("rimshot\n")
-  end
+  samples = {
+    [0xFF0000] = "haha";
+    [0x00FF00] = "fail";
+    [0x0000FF] = "haha";
+  }
+  sample = samples[r*0x10000 + g*0x100 + b]
+  socket:send((sample and sample or "rimshot") .. "\n")
 
   socket:close()
 end
@@ -45,7 +40,7 @@ function button_callback (level)
   gpio.mode(BUTTON, gpio.INPUT) -- disable interrupt
   tmr.alarm(0, 300, tmr.ALARM_SINGLE, button_init)
   if level == 0 then
-    node.task.post(rimshot)
+    node.task.post(trigger_effect)
   end
 end
 
